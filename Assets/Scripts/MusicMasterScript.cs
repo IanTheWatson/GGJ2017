c604488c5bb12e72f0dfe349486ad26654846b12
IanTheWatson <ian@devolutious.co.nz>
2017-01-20 21:53:54 +1300

    Added placeholder player and started on movement ability

---
+++ Assets/Scripts/MusicMasterScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicMasterScript : MonoBehaviour {
+
+    public int BPM;
+
+    public float BPS
+    {
+     get
+        {
+            return BPM / 60;
+        }
+    }
+
+    public float MovementSpeedPerTick
+    {
+        get
+        {
+            return BPS * Time.fixedDeltaTime;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public MusicMasterScript master;
+    public Camera playerCamera;
+
+    bool _moving = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.right * master.MovementSpeedPerTick);
+        playerCamera.transform.position = new Vector3(transform.position.x, transform.position.y, playerCamera.transform.position.z);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (!_moving)
+            {
+                _moving = true;
+                transform.Translate(Vector3.up * 0.5f);
+            }
+        }
+        else
+        {
+            _moving = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (!_moving)
+            {
+                _moving = true;
+                transform.Translate(Vector3.down * 0.5f);
+            }
+        }
+        else
+        {
+            _moving = false;
+        }
+    }
+}
